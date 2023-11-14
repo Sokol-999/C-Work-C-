75c87b30f28f5e4f01f5fe33daf6837c88a76e16
Sokolov Ilya <sokolov.ilia.2000@mail.ru>
2023-11-14 21:25:28 +0300

    wrote and added the program file and folder

---
+++ program/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] arr = { "hello", "2", "world", ":-)" };
+string[] newArr = new string[arr.Length];
+int i = 0;
+int number = 3;
+
+foreach (string str in arr)
+{
+    if (str.Length <= number)
+    {
+        newArr[i] = str;
+        i++;
+    }
+}
+
+var str1 = string.Join(" " ,newArr);
+System.Console.WriteLine("[ " + str1 + "]");
+System.Console.WriteLine(newArr[1]);
